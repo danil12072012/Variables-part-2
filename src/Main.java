a2bedf66d21dd11a2ea88a43d37c946032f0b91f
mycomputer <Danil12.07.2012@gail.com>
2022-12-30 14:57:59 +0300

    Задача 4

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,16 @@
 public class Main {
     public static void main(String[] args) {
 
-        byte studentsOfLyudmilaPavlovna = 23;
-        byte studentsOfAnnaSergeevna = 27;
-        byte  studentsOfEkaterinaAndreevna = 30;
-        short sheetsOfPaperTotal = 480;
-        int sheetsStudent = sheetsOfPaperTotal /(studentsOfLyudmilaPavlovna + studentsOfAnnaSergeevna + studentsOfEkaterinaAndreevna);
-        System.out.println("На каждого ученика рассчитано " + sheetsStudent  + "  листов бумаги.");
+        byte efficiencyTwoMinutes = 16;
+        int efficiency = efficiencyTwoMinutes / 2;
+        int producedTwentyMinutes = 20 * efficiency;
+        int produceDay = 24 * 60 * efficiency;
+        int produceThreeDay = 3 * 24 * 60 * efficiency;
+        int produceMonth = 30 * 24 * 60 * efficiency;
+        System.out.println("За 20 минут машина произвела " +  producedTwentyMinutes + " штук бутылок.");
+        System.out.println("За сутки машина произвела " +  produceDay + " штук бутылок.");
+        System.out.println("За 3 дня машина произвела " +  produceThreeDay + " штук бутылок.");
+        System.out.println("За 1 месяц машина произвела " +  produceMonth + " штук бутылок.");
+
     }
 }
