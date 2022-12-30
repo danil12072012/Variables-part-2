f362236201da16627655d592f0246c9e407ef2c8
mycomputer <Danil12.07.2012@gail.com>
2022-12-30 15:03:14 +0300

    Задача 6

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,17 @@
 public class Main {
     public static void main(String[] args) {
 
-        byte paintOneClassWhite = 2;
-        byte paintOneClassBrown = 4;
-        byte paintTotal = 120;
-        int paintOneClass = paintOneClassBrown + paintOneClassWhite;
-        int classTotal = paintTotal / paintOneClass;
-        int paintWhite = classTotal * paintOneClassWhite;
-        int paintBrown = classTotal * paintOneClassBrown;
-        System.out.println("В школе, где " + classTotal + " классов, нужно " + paintWhite + " банок белой краски и " + paintBrown + " банок коричневой краски.");
+        byte bananaOneWeight = 80;
+        byte bananaQuantity = 5;
+        byte milkOneWeight = 105;
+        byte milkQuantity = 2;
+        byte iceCreamOneWeight = 100;
+        byte iceCreamQuantity = 2;
+        byte eggOneWeight = 70;
+        byte eggQuantity = 4;
+        int breakfastGram = bananaOneWeight * bananaQuantity + milkOneWeight * milkQuantity + iceCreamOneWeight * iceCreamQuantity + eggOneWeight * eggQuantity;
+        int breakfastKilogram = breakfastGram / 1000;
+        System.out.println("Вес завтрака спортсмена составил " + breakfastGram + " грамм, что соответствует " + breakfastKilogram + " килограмм.");
 
 
     }
