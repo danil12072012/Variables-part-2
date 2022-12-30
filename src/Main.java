4b722d96006295ab410af4cf403d53cfe5b38ee5
mycomputer <Danil12.07.2012@gail.com>
2022-12-30 14:49:57 +0300

    Задача 2

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,14 @@
 public class Main {
     public static void main(String[] args) {
 
-        byte a = 52;
-        short b = 368;
-        int c = 5225;
-        long d = 44153L;
-        float e = 2.5f;
-        double f = 3.5448;
-        System.out.println ("Значение переменной a с типом byte равно" + a);
-        System.out.println("Значение переменной b с типом short равно" + b);
-        System.out.println("Значение переменной c с типом int равно" + c);
-        System.out.println("Значение переменной d с типом long равно" + d);
-        System.out.println("Значение переменной e с типом float равно" + e);
-        System.out.println("Значение переменной f с типом double равно" + f);
+        float a = 27.12f;
+        long b = 987678965549L;
+        float c = 2.786f;
+        short d = 569;
+        short e = -159;
+        short f = 27897;
+        byte g = 67;
+
         ;
     }
 }
